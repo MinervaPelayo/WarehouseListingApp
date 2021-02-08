1c9948ef6e80364d4f4a4436442096949ecbad02
MinervaPelayo <47009578+MinervaPelayo@users.noreply.github.com>
2021-02-08 14:52:49 +0200

    Change request

--- warehouse-app/src/App.js
+++ warehouse-app/src/App.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, {useState, useEffect} from 'react';
 import NavBar from './components/NavBar';
+import Message from './components/Message';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [gloveData, setGloveData] = useState([]);
+  const [facemaskData, setFacemaskData] = useState([]);
+  const [beanieData, setBeanieData] = useState([]);
+  const [error, setError] = useState('')
+  const baseUrl = 'https://bad-api-assignment.reaktor.com/v2';
+  
+  useEffect(() => {
+    const requestOne = new Request(baseUrl, {
+      method: 'GET',
+      mode:'no-cors',
+    });
+    const requestTwo = fetch(`${baseUrl}/products/facemasks`, { mode: 'no-cors'});
+    const requestThree = fetch(`${baseUrl}/products/beanies`, { mode: 'no-cors'});
+
+    Promise
+      .all([requestOne,requestTwo,requestThree])
+    //axios
+      //.all([requestOne, requestTwo, requestThree])
+      .then((responses) => {
+        console.log(responses)
+        //setGloveData(responses[0].data);
+        //setFacemaskData(responses[1].data);
+        //setBeanieData(responses[2].data);
+      })
+      .catch(errors => {
+        setError(
+          `Data retrieval error, please try again later`
+        )
+        setTimeout(() => {
+          setError(null)
+        }, 5000)
+        console.error("Error: " + errors);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <NavBar />
+      <NavBar gloveData={gloveData} facemaskData={facemaskData} beanieData={beanieData} />
+      <Message message={error} />
     </div>
   );
 };
--- warehouse-app/src/components/Glove.js
+++ warehouse-app/src/components/Glove.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import ReactTable from 'react-table-6';
+import 'react-table-6/react-table.css';
 
-const Glove = () => {
+const Glove = (props) => {
+    const columns=[
+        { Header:'Product Name',
+         accessor:'product'
+        },
+        { Header:'Availability',
+            accessor:'available'
+        }
+    ];
+
+    const data = [{
+        "product": "Myproduct",
+        "available": "In stock"
+      },
+    {
+        "product": "Other product",
+        "available": "Out of stock"
+    }]
+
+    const filterCaseInsensitive = (filter, row) => {
+        const id = filter.pivotId || filter.id;
+        const content = row[id];
+        if (typeof content !== 'undefined') {
+            // filter by text in the table or if it's a object, filter by key
+            if (typeof content === 'object' && content !== null && content.key) {
+                return String(content.key).toLowerCase().includes(filter.value.toLowerCase());
+            } else {
+                return String(content).toLowerCase().includes(filter.value.toLowerCase());
+            }
+        }
+        return true;
+    }
 
     return (
         <div>
-            <p>Here goes list of gloves</p>
+           <ReactTable 
+                data={data} 
+                columns={columns}
+                filterable={true}
+                defaultPageSize={10}
+                defaultFilterMethod={filterCaseInsensitive}/>
         </div>
     );
 }
